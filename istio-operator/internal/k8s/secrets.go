1820d4e4e21ebb2e9778a1a4628e7811c00893e3
Andrejs Ponatovskis <93515289+AndrejsPon00@users.noreply.github.com>
2026-01-30 18:11:33 +0200

    fix: adopted cluster secret name hardcoded assumption (#32)

--- istio-operator/internal/k8s/secrets.go
+++ istio-operator/internal/k8s/secrets.go
@@ -3,9 +3,10 @@ package k8s
 import (
 	"context"
 	"fmt"
-	"strings"
 
 	kcmv1beta1 "github.com/K0rdent/kcm/api/v1beta1"
+	"github.com/k0rdent/istio/istio-operator/internal/controller/utils"
+	crds "github.com/k0rdent/istio/istio-operator/internal/crd"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -14,8 +15,7 @@ import (
 const (
 	DefaultKCMSystemNamespace = "kcm-system"
 
-	AdoptedClusterSecretSuffix = "kubeconf"
-	ClusterSecretSuffix        = "kubeconfig"
+	ClusterSecretSuffix = "kubeconfig"
 )
 
 func GetSecret(ctx context.Context, k8sClient client.Client, name string, namespace string) (*corev1.Secret, error) {
@@ -31,9 +31,34 @@ func GetSecretValue(secret *corev1.Secret) []byte {
 	return nil
 }
 
-func GetSecretName(cd *kcmv1beta1.ClusterDeployment) string {
-	if strings.Contains(cd.Spec.Template, "adopted") {
-		return fmt.Sprintf("%s-%s", cd.Name, AdoptedClusterSecretSuffix)
+// GetKubeconfigSecretName returns the name of the secret containing the kubeconfig
+// for the given ClusterDeployment. The provided k8sClient must be a management cluster client,
+// because this function reads the Credential resource, which is stored in the management cluster.
+func GetKubeconfigSecretName(ctx context.Context, k8sClient client.Client, cd *kcmv1beta1.ClusterDeployment) (string, error) {
+	if utils.IsAdopted(cd) {
+		return GetAdoptedClusterSecretName(ctx, k8sClient, cd)
 	}
+	return GetCloudClusterSecretName(cd), nil
+}
+
+func GetAdoptedClusterSecretName(ctx context.Context, k8sClient client.Client, cd *kcmv1beta1.ClusterDeployment) (string, error) {
+	cred := new(crds.Credential)
+	namespacedName := types.NamespacedName{
+		Name:      cd.Spec.Credential,
+		Namespace: cd.Namespace,
+	}
+
+	if err := k8sClient.Get(ctx, namespacedName, cred); err != nil {
+		return "", fmt.Errorf("failed to get credential: %w", err)
+	}
+
+	if cred.Spec.IdentityRef.Kind != "Secret" {
+		return "", fmt.Errorf("unsupported Credential IdentityRef kind %s for adopted cluster", cred.Spec.IdentityRef.Kind)
+	}
+
+	return cred.Spec.IdentityRef.Name, nil
+}
+
+func GetCloudClusterSecretName(cd *kcmv1beta1.ClusterDeployment) string {
 	return fmt.Sprintf("%s-%s", cd.Name, ClusterSecretSuffix)
 }
