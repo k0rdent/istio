0b01288c7b90a8ddf7c8029a4602d1ab00235700
Andrejs Ponatovskis <93515289+AndrejsPon00@users.noreply.github.com>
2026-05-12 11:56:33 +0300

    ci: add workflow to lint and test Istio operator and Helm chart (#31)
    * ci: add workflow to lint and test Istio operator and Helm chart
    
    * apply review suggestion
    
    * add adopted cluster upgrade ci test
    
    * fix adopted cluster template
    
    * fix adopted cluster name
    
    * add support bundle
    
    * fix script name
    
    * fix wait condition
    
    * wait until the services are installed
    
    * fix kcm upgrade
    
    * add build_images.yml workflow
    
    * fix workflow trigger
    
    * build and test on merge

---
+++ scripts/create_support_bundle.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# This script can be executed with `make support-bundle` or directly.
+#
+# Optional env vars:
+# * KUBECTL - default: `kubectl`
+# * KUBECTL_CONTEXT - default: current, as in `kubectl config current-context`
+# * SUPPORT_BUNDLE_CLI - default: `kubectl-support_bundle`
+# * SUPPORT_BUNDLE_OUTPUT - default: `support-bundle-{timestamp}.tar.gz`
+
+sb="${SUPPORT_BUNDLE_CLI:-kubectl-support_bundle}"
+command -v "$sb" || {
+  echo "$sb is not found."
+  echo 'Please run `make support-bundle` or install this plugin first:'
+  echo 'https://docs.replicated.com/vendor/support-bundle-generating#prerequisite-install-the-support-bundle-plugin'
+  exit 1
+}
+
+k="${KUBECTL:-kubectl}"
+ctx="${KUBECTL_CONTEXT:-}"  # Empty string means the current context.
+
+dev_dir=$(dirname "$0")/../dev
+mkdir -p "$dev_dir"
+config_file="$dev_dir/support-bundle.yaml"
+
+cat >"$config_file" <<EOF
+apiVersion: troubleshoot.sh/v1beta2
+kind: SupportBundle
+metadata:
+  name: support-bundle
+spec:
+  collectors:
+EOF
+
+"$k" --context "$ctx" get ns \
+  -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' \
+  | while read -r ns; do cat >>"$config_file" <<EOF
+    - logs:
+        namespace: "$ns"
+        name: "logs/$ns"
+EOF
+done
+
+"$sb" --context "$ctx" -o "${SUPPORT_BUNDLE_OUTPUT:-}" "$config_file"
+
+echo '
+To analyze this support bundle:
+* Run: scripts/support-bundle-analyzer.py support-bundle-{timestamp}.tar.gz
+* Install `sbctl`: https://github.com/replicatedhq/sbctl/blob/main/README.md
+* Run: sbctl shell -s support-bundle-{timestamp}.tar.gz
+* Use: kubectl, k9s, etc.
+'
